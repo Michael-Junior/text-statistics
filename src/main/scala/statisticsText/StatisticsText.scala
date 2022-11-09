fba95e214662704b244db86706785fadfa818604
michael-junior <michaeljunioroliveira@gmail.com>
2022-11-09 08:23:53 -0300

    Implementação dos principais metodos para geração de estatisticas

--- src/main/scala/statisticsText/StatisticsText.scala
+++ src/main/scala/statisticsText/StatisticsText.scala
@@ -3,58 +3,68 @@ package statisticsText
 import java.io.{BufferedWriter, File, FileWriter}
 import java.nio.file.{Files, Path}
 import java.time.LocalDate
-import java.time.format.{DateTimeFormatter, FormatStyle}
-import scala.util.{Try, Using}
+import java.time.format.DateTimeFormatter
+import scala.io.Source
+import scala.util.{Failure, Success, Try}
 
 case class Statistics(inputFile: String,
-                      outputFile: String,
                       dateCreation: String,
                       dateModification: String,
                       numberLines: String,
-                      numberWords: String,
-                      numberSpaces: String)
-
+                      //                      numberWords: String,
+                      //                      numberSpaces: String
+                     )
 
 class StatisticsText {
 
-  def staticsText(path_in: Path, path_out: Path): Try[Unit] = {
+  def mappingStatistics(path_in: Path): Statistics = {
+
+    //val dirFile: String = String(Files.readAllBytes(path_in))
+    val dateCreation = LocalDate.now.format(DateTimeFormatter.ofPattern("dd/MM/yyyy"))
+    val dateModification = LocalDate.now.format(DateTimeFormatter.ofPattern("dd/MM/yyyy"))
+    val bufferedSource = Source.fromFile(File(path_in.toString))
+    val numberLines = bufferedSource.getLines().size
+
+    Statistics(path_in.toString, dateCreation, dateModification, numberLines.toString)
+  }
+
+  def defineOutput(path_in: Path, path_out: Path): Try[Unit] = {
     Try {
+      val createReportFile: Boolean = path_out != null
+      val statistics: Statistics = mappingStatistics(path_in)
 
-      val dirFile = new String(Files.readAllBytes(path_in))
-
-      val teste = if path_out == null then "" else path_out
-
-      val createReport: Boolean = path_out != null
-      val date = LocalDate.now.format(DateTimeFormatter.ofPattern("dd/MM/yyyy"))
-      val statics = Statistics(path_in.toString, teste.toString, date, date, "3", "4", "5")
-
-      if (createReport) {
-        report(statics)
-      } else {
-        println("----------------------------------------")
-        println(statics.dateCreation)
-        println("----------------------------------------")
-        println(dirFile)
-        println("----------------------------------------")
-      }
+      if createReportFile then reportStatistics(statistics, path_out)
+      else standardOutput(statistics)
     }
   }
 
-  def report(statistics: Statistics): Unit = {
+  def reportStatistics(statistics: Statistics, path_out: Path): Try[Unit] = {
+    Try {
 
-    val pathFileLog = File(s"${statistics.outputFile}/logs.txt")
-    if (!pathFileLog.exists) pathFileLog.createNewFile
+      val statisticsFile = File(s"$path_out/statistics.txt")
+      if (!statisticsFile.exists) statisticsFile.createNewFile
 
-    val fileWriter = FileWriter(pathFileLog, true)
-    val bufferedWriter = BufferedWriter(fileWriter)
+      val fileWriter = FileWriter(statisticsFile, true)
+      val bufferedWriter = BufferedWriter(fileWriter)
 
-    bufferedWriter.write(statistics.dateCreation)
-    bufferedWriter.newLine()
-    bufferedWriter.close()
-    fileWriter.close()
+      bufferedWriter.write(s"\nARQUIVO: ${statistics.inputFile}\n")
+      bufferedWriter.write(s"DATA DE CRIAÇÃO: ${statistics.dateCreation}\n")
+      bufferedWriter.write(s"DATA DE MODIFICAÇÃO: ${statistics.dateModification}\n")
+      bufferedWriter.write(s"NÚMERO DE LINHAS: ${statistics.numberLines}\n")
+      bufferedWriter.close()
+      fileWriter.close()
+    }
+  }
+
+  def standardOutput(statistics: Statistics): Unit = {
+    println("----------------------------------------------")
+    println(s"ARQUIVO: ${statistics.inputFile}")
+    println(s"DATA DE CRIAÇÃO: ${statistics.dateCreation}")
+    println(s"DATA DE MODIFICAÇÃO: ${statistics.dateModification}")
+    println(s"NÚMERO DE LINHAS: ${statistics.numberLines}")
+    println("----------------------------------------------")
   }
 }
-
 
 object StatisticsText {
 
@@ -62,12 +72,21 @@ object StatisticsText {
 
     val parser = argparse.default.ArgumentParser(description = "Programa para Estatísticas de texto")
 
-    val path_in = parser.requiredParam[java.nio.file.Path]("--in", help = "Diretório de entrada do arquivo .TXT")
-    val path_out = parser.param[java.nio.file.Path]("--out", default = null, help = "Se informado um diretório," +
-      " será gerado um aquivo .TXT com as Estatísticas de texto")
+    val path_in = parser.requiredParam[Path]("--in", help = "Diretório de entrada do arquivo .TXT")
+    val path_out = parser.param[Path]("--out", default = null, help = "Se informado um diretório, será gerado" +
+      " um aquivo .TXT com as Estatísticas de texto")
 
     parser.parseOrExit(args)
 
-    (new StatisticsText).staticsText(path_in.value, path_out.value)
+    (new StatisticsText).defineOutput(path_in.value, path_out.value) match
+      case Success(_) if path_out.value == null =>
+        println("\nStatistics generated successfully!\n")
+        System.exit(0)
+      case Success(_) =>
+        println(s"\nStats file successfully generated at: ${path_out.value}/statistics.txt\n")
+        System.exit(0)
+      case Failure(exception) =>
+        println(s"\nFile not generated: ${exception.toString}\n")
+        System.exit(1)
   }
 }
