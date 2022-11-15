935a1d46100c337244b1e9bfa01351f8aafc3bbf
michael-junior <michaeljunioroliveira@gmail.com>
2022-11-15 13:46:21 -0300

    Implementação inicial para as estatisticas de linhas e palavras

--- src/main/scala/statisticsText/StatisticsText.scala
+++ src/main/scala/statisticsText/StatisticsText.scala
@@ -4,8 +4,6 @@ import java.io.{BufferedWriter, File, FileWriter}
 import java.nio.file.{Files, Path, Paths}
 import java.time.LocalDate
 import java.time.format.DateTimeFormatter
-import java.util
-import scala.Some
 import scala.collection.MapView
 import scala.io.{BufferedSource, Source}
 import scala.util.matching.Regex
@@ -17,7 +15,8 @@ case class Statistics(inputFile: String,
                       numberLines: String,
                       numberWords: String,
                       numberSpaces: String,
-                      lineGreeting: String)
+                      lineGreeting: String,
+                      wordsGreeting: String)
 
 class StatisticsText {
 
@@ -27,16 +26,47 @@ class StatisticsText {
     val dateCreation: String = LocalDate.now.format(DateTimeFormatter.ofPattern("dd/MM/yyyy"))
     val dateModification: String = LocalDate.now.format(DateTimeFormatter.ofPattern("dd/MM/yyyy"))
     val numberLines: String = dirFile.lines().count().toString
-    val numberWords: String = dirFile.split("\\b([A-Za-z\\-])").length.toString
+    val numberWords: String = dirFile.split("\\S+").length.toString
     val numberSpaces: String = dirFile.split("\\b(\\s)\\b").length.toString //validar expressão
 
     val bufferedSource: BufferedSource = Source.fromFile(File(path_in.toString))
     val linesLengthList: Seq[Int] = bufferedSource.getLines().toSeq.map(x => x.split("\\W").mkString(" ").length)
-    val linesGreeting = statisticsLines(linesLengthList)
+    val linesGreeting: String = statisticsLines(linesLengthList)
+
+    val bufferedSourceW: BufferedSource = Source.fromFile(File(path_in.toString))
+    val wordsLengthList: Seq[String] = bufferedSourceW.getLines().toSeq.map(x => x.split("\\s").mkString(" "))
+    val words: Seq[Seq[String]] = wordsLengthList.map(f => f.split(" ").map(_.trim))
+    val maisUm: Seq[Seq[Int]] = words.map(f => f.map(f => f.length))
+
+    val wordsGreeting: String = statisticsWords(maisUm)
+    val wordsGreetingWords: String = statisticsWords(maisUm)
 
     bufferedSource.close()
-    Statistics(path_in.toString, dateCreation, dateModification, numberLines, numberWords, numberSpaces, linesGreeting)
+    bufferedSourceW.close()
+
+    Statistics(path_in.toString, dateCreation, dateModification, numberLines, numberWords, numberSpaces, linesGreeting, wordsGreeting)
   }
+
+  def statisticsWords(linesLengthList: Seq[Seq[Int]]): String = {
+
+    var str = ""
+
+    for (i <- linesLengthList) {
+      val listLines: MapView[Int, Int] = i.groupBy(identity).mapValues(f = _.size)
+
+      val concatLength: (String, (Int, Int)) => String = (a, b) => a + s"comprimento de linha [${b._1}]:${b._2}\n"
+      val linesConcatLength: String = listLines.foldLeft("")(concatLength)
+
+      val concatLengthAverage: (Int, (Int, Int)) => Int = (a, b) => a + b._1 / listLines.size
+      val linesConcatLengthAverage: Int = listLines.foldLeft(0)(concatLengthAverage)
+
+      val statisticsLines: String = linesConcatLength.concat(s"comprimento médio das linhas: $linesConcatLengthAverage")
+
+      str = statisticsLines
+    }
+    str
+  }
+
 
   def statisticsLines(linesLengthList: Seq[Int]): String = {
 
@@ -46,12 +76,13 @@ class StatisticsText {
     val linesConcatLength: String = listLines.foldLeft("")(concatLength)
 
     val concatLengthAverage: (Int, (Int, Int)) => Int = (a, b) => a + b._1 / listLines.size
-    val linesConcatLengthAverage: Int  = listLines.foldLeft(0)(concatLengthAverage)
+    val linesConcatLengthAverage: Int = listLines.foldLeft(0)(concatLengthAverage)
 
     val statisticsLines: String = linesConcatLength.concat(s"comprimento médio das linhas: $linesConcatLengthAverage")
 
     statisticsLines
   }
+
 
   def defineOutput(path_in: Path, path_out: Path): Try[Unit] = {
     Try {
@@ -72,13 +103,15 @@ class StatisticsText {
       val fileWriter = FileWriter(statisticsFile, false)
       val bufferedWriter = BufferedWriter(fileWriter)
 
-      bufferedWriter.write(s"\nARQUIVO: ${statistics.inputFile}\n")
+      bufferedWriter.write("---------Statistics Text---------\n")
+      bufferedWriter.write(s"ARQUIVO: ${statistics.inputFile}\n")
       bufferedWriter.write(s"DATA DE CRIAÇÃO: ${statistics.dateCreation}\n")
       bufferedWriter.write(s"DATA DE MODIFICAÇÃO: ${statistics.dateModification}\n")
       bufferedWriter.write(s"NÚMERO DE LINHAS: ${statistics.numberLines}\n")
       bufferedWriter.write(s"NÚMERO DE PALAVRAS: ${statistics.numberWords}\n")
       bufferedWriter.write(s"NÚMERO DE ESPAÇOS: ${statistics.numberSpaces}\n")
       bufferedWriter.write(s"\n${statistics.lineGreeting}")
+      bufferedWriter.write(s"\n${statistics.wordsGreeting}")
 
       bufferedWriter.close()
       fileWriter.close()
@@ -86,15 +119,15 @@ class StatisticsText {
   }
 
   def standardOutput(statistics: Statistics): Unit = {
-    println("----------------------------------------------")
+    println("---------Statistics Text---------")
     println(s"ARQUIVO: ${statistics.inputFile}")
     println(s"DATA DE CRIAÇÃO: ${statistics.dateCreation}")
     println(s"DATA DE MODIFICAÇÃO: ${statistics.dateModification}")
     println(s"NÚMERO DE LINHAS: ${statistics.numberLines}")
     println(s"NÚMERO DE PALAVRAS: ${statistics.numberWords}")
-    println(s"NÚMERO DE ESPAÇOS: ${statistics.numberSpaces}")
     println(s"NÚMERO DE ESPAÇOS: ${statistics.numberSpaces}\n")
     println(statistics.lineGreeting)
+    println(s"\n${statistics.wordsGreeting}")
     println("----------------------------------------------")
   }
 }
@@ -112,11 +145,12 @@ object StatisticsText {
     parser.parseOrExit(args)
 
     (new StatisticsText).defineOutput(path_in.value, path_out.value) match
-      case Success(_) if path_out.value == null =>
-        println("Statistics generated successfully!\n")
-        System.exit(0)
       case Success(_) =>
-        println(s"\nStats file successfully generated at: ${path_out.value}\n")
+        if (path_out.value == null) {
+          println("Statistics generated successfully!\n")
+          System.exit(0)
+        }
+        else println(s"\nStats file successfully generated at: ${path_out.value}\n")
         System.exit(0)
       case Failure(exception) =>
         println(s"\nFile not generated: ${exception.toString}\n")
