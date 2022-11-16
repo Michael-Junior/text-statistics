f58fd75c6f0899ede28dffff1a655f4feb4b108c
michael-junior <michaeljunioroliveira@gmail.com>
2022-11-16 10:43:43 -0300

    Ajustes nos metodos e implementação da contagem de repetição das palavras

--- src/main/scala/statisticsText/StatisticsText.scala
+++ src/main/scala/statisticsText/StatisticsText.scala
@@ -16,7 +16,8 @@ case class Statistics(inputFile: String,
                       numberWords: String,
                       numberSpaces: String,
                       lineGreeting: String,
-                      wordsGreeting: String)
+                      wordsGreeting: String,
+                      wordsRepetition: String)
 
 class StatisticsText {
 
@@ -25,64 +26,45 @@ class StatisticsText {
     val dirFile: String = String(Files.readAllBytes(path_in))
     val dateCreation: String = LocalDate.now.format(DateTimeFormatter.ofPattern("dd/MM/yyyy"))
     val dateModification: String = LocalDate.now.format(DateTimeFormatter.ofPattern("dd/MM/yyyy"))
-    val numberLines: String = dirFile.lines().count().toString
-    val numberWords: String = dirFile.split("\\S+").length.toString
-    val numberSpaces: String = dirFile.split("\\b(\\s)\\b").length.toString //validar expressão
+    val text: Seq[String] = dirFile.split("\n").toSeq
 
-    val bufferedSource: BufferedSource = Source.fromFile(File(path_in.toString))
-    val linesLengthList: Seq[Int] = bufferedSource.getLines().toSeq.map(x => x.split("\\W").mkString(" ").length)
-    val linesGreeting: String = statisticsLines(linesLengthList)
+    val numberLines: String = text.size.toString
+    val numberWords: String = text.map(f => f.split("\\s+").length).sum.toString
+    val numberSpaces: String = text.map(f => f.split("\\b(\\s)\\b").length).sum.toString
 
-    val bufferedSourceW: BufferedSource = Source.fromFile(File(path_in.toString))
-    val wordsLengthList: Seq[String] = bufferedSourceW.getLines().toSeq.map(x => x.split("\\s").mkString(" "))
-    val words: Seq[Seq[String]] = wordsLengthList.map(f => f.split(" ").map(_.trim))
-    val maisUm: Seq[Seq[Int]] = words.map(f => f.map(f => f.length))
+    val linesLengthList: Seq[Int] = text.map(x => x.split("\\s").mkString(" ").length)
+    val linesGreeting: String = getStatisticsLines(linesLengthList, "linha")
 
-    val wordsGreeting: String = statisticsWords(maisUm)
-    val wordsGreetingWords: String = statisticsWords(maisUm)
+    val wordsLengthList: Seq[Int] = dirFile.split("\\s+").map(f => f.length).toSeq
+    val wordsGreeting: String = getStatisticsLines(wordsLengthList, "palavra")
 
-    bufferedSource.close()
-    bufferedSourceW.close()
+    val wordsRepeated: String = getWordsRepetition(dirFile.split("\\s+"))
 
-    Statistics(path_in.toString, dateCreation, dateModification, numberLines, numberWords, numberSpaces, linesGreeting, wordsGreeting)
+    Statistics(path_in.toString, dateCreation, dateModification, numberLines, numberWords, numberSpaces, linesGreeting,
+      wordsGreeting, wordsRepeated)
   }
 
-  def statisticsWords(linesLengthList: Seq[Seq[Int]]): String = {
+  def getWordsRepetition(str: Seq[String]): String ={
 
-    var str = ""
-
-    for (i <- linesLengthList) {
-      val listLines: MapView[Int, Int] = i.groupBy(identity).mapValues(f = _.size)
-
-      val concatLength: (String, (Int, Int)) => String = (a, b) => a + s"comprimento de linha [${b._1}]:${b._2}\n"
-      val linesConcatLength: String = listLines.foldLeft("")(concatLength)
-
-      val concatLengthAverage: (Int, (Int, Int)) => Int = (a, b) => a + b._1 / listLines.size
-      val linesConcatLengthAverage: Int = listLines.foldLeft(0)(concatLengthAverage)
-
-      val statisticsLines: String = linesConcatLength.concat(s"comprimento médio das linhas: $linesConcatLengthAverage")
-
-      str = statisticsLines
-    }
-    str
+    val strList: Map[String, Int] = str.groupBy(identity).map(f => (f._1, f._2.length))
+    val contatWordsQtd: (String, (String, Int)) => String = (a, b) => a + s"[${b._1}]:${b._2}\n"
+    strList.foldLeft("")(contatWordsQtd)
   }
 
-
-  def statisticsLines(linesLengthList: Seq[Int]): String = {
+  def getStatisticsLines(linesLengthList: Seq[Int], title: String): String = {
 
     val listLines: MapView[Int, Int] = linesLengthList.groupBy(identity).mapValues(f = _.size)
 
-    val concatLength: (String, (Int, Int)) => String = (a, b) => a + s"comprimento de linha [${b._1}]:${b._2}\n"
+    val concatLength: (String, (Int, Int)) => String = (a, b) => a + s"comprimento de ${title} [${b._1}]: ${b._2}\n"
     val linesConcatLength: String = listLines.foldLeft("")(concatLength)
 
     val concatLengthAverage: (Int, (Int, Int)) => Int = (a, b) => a + b._1 / listLines.size
     val linesConcatLengthAverage: Int = listLines.foldLeft(0)(concatLengthAverage)
 
-    val statisticsLines: String = linesConcatLength.concat(s"comprimento médio das linhas: $linesConcatLengthAverage")
+    val statisticsLines: String = linesConcatLength.concat(s"comprimento médio das ${title}s: $linesConcatLengthAverage")
 
     statisticsLines
   }
-
 
   def defineOutput(path_in: Path, path_out: Path): Try[Unit] = {
     Try {
@@ -109,9 +91,12 @@ class StatisticsText {
       bufferedWriter.write(s"DATA DE MODIFICAÇÃO: ${statistics.dateModification}\n")
       bufferedWriter.write(s"NÚMERO DE LINHAS: ${statistics.numberLines}\n")
       bufferedWriter.write(s"NÚMERO DE PALAVRAS: ${statistics.numberWords}\n")
-      bufferedWriter.write(s"NÚMERO DE ESPAÇOS: ${statistics.numberSpaces}\n")
-      bufferedWriter.write(s"\n${statistics.lineGreeting}")
-      bufferedWriter.write(s"\n${statistics.wordsGreeting}")
+      bufferedWriter.write(s"NÚMERO DE ESPAÇOS: ${statistics.numberSpaces}\n\n")
+      bufferedWriter.write(statistics.lineGreeting)
+      bufferedWriter.write(s"\n\n")
+      bufferedWriter.write(statistics.wordsGreeting)
+      bufferedWriter.write(s"\n\n")
+      bufferedWriter.write(statistics.wordsRepetition)
 
       bufferedWriter.close()
       fileWriter.close()
@@ -119,7 +104,7 @@ class StatisticsText {
   }
 
   def standardOutput(statistics: Statistics): Unit = {
-    println("---------Statistics Text---------")
+    println("----------------Statistics Text----------------")
     println(s"ARQUIVO: ${statistics.inputFile}")
     println(s"DATA DE CRIAÇÃO: ${statistics.dateCreation}")
     println(s"DATA DE MODIFICAÇÃO: ${statistics.dateModification}")
@@ -128,7 +113,8 @@ class StatisticsText {
     println(s"NÚMERO DE ESPAÇOS: ${statistics.numberSpaces}\n")
     println(statistics.lineGreeting)
     println(s"\n${statistics.wordsGreeting}")
-    println("----------------------------------------------")
+    println(s"\n${statistics.wordsRepetition}")
+    println("-----------------------------------------------")
   }
 }
 
